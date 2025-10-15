b13203b264a9c85116fb52bbca5b33fc3e7405bc
Shayan Tehrani <shayanthrn@gmail.com>
2025-10-15 17:26:09 -0400

    add horizantal layout + utlizing OO for better code structure.

--- main.py
+++ main.py
@@ -29,20 +29,44 @@ class UIElement:
         text = font.render(str(self.id), True, (0, 0, 0))
         screen.blit(text, (x + 5, y + 5))
 
-class VerticalLinearLayout:
-    def __init__(self,x,y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+
+class Layout(UIElement):
+    def __init__(self,width, height,id=0):
+        super().__init__(width,height)
         self.elements = []
-    def draw(self,screen):
-        current_y = self.y
+
+    def addElement(self,element):
+        if(element.width > self.width or element.height > self.height):
+            printError("Element exceeds layout bounds")
+        self.elements.append(element)
+
+
+class VerticalLinearLayout(Layout):
+
+    def __init__(self,width, height,id=0):
+        super().__init__(width,height,id)
+
+    def draw(self,screen,x,y):
+        current_y = y
         for element in self.elements:
-            if(current_y + element.height > self.y + self.height or element.width > self.width):
+            if(current_y + element.height > y + self.height or element.width > self.width):
                 printError("Element exceeds layout bounds")
-            element.draw(screen,self.x,current_y)
+            element.draw(screen,x,current_y)
             current_y += element.height
+
+
+class HorizantalLinearLayout(Layout):
+
+    def __init__(self,width, height,id=0):
+        super().__init__(width,height,id)
+        
+    def draw(self,screen,x,y):
+        current_x = x
+        for element in self.elements:
+            if(current_x + element.width > x + self.width or element.height > self.height):
+                printError("Element exceeds layout bounds")
+            element.draw(screen,current_x,y)
+            current_x += element.width
 
 def checkQuitEvent():
     for event in pygame.event.get():
@@ -75,13 +99,34 @@ while True:
     # board.drawBoard(50, 450)
     # board.drawBoard(250, 450)
     # board.drawBoard(450, 450)
-    mainLayout = VerticalLinearLayout(0, 0,SCREEN_WIDTH, SCREEN_HEIGHT)
-    mainLayout.elements.append(UIElement(100,100,1))
-    mainLayout.elements.append(UIElement(SCREEN_WIDTH,100,2))
-    mainLayout.elements.append(UIElement(SCREEN_WIDTH,200,3))
-    mainLayout.elements.append(UIElement(600,200,4))
-    mainLayout.elements.append(UIElement(600,300,5))
-    mainLayout.draw(screen)
+
+
+
+
+    # mainLayout = VerticalLinearLayout(SCREEN_WIDTH, SCREEN_HEIGHT)
+    # header = UIElement(SCREEN_WIDTH,50,1)
+    # mainLayout.addElement(header)
+    # midSection = HorizantalLinearLayout(SCREEN_WIDTH, 800)
+    # ele1 = UIElement(200,800,4)
+    # ele2 = UIElement(200,800,5)
+    # ele3 = UIElement(200,800,6)
+    # ele4 = UIElement(200,800,7)
+    # ele5 = UIElement(200,800,8)
+    # midSection.addElement(ele1)
+    # midSection.addElement(ele2)
+    # midSection.addElement(ele3)
+    # midSection.addElement(ele4)
+    # midSection.addElement(ele5)
+    # mainLayout.addElement(midSection)
+    # footer = UIElement(SCREEN_WIDTH,50,3)
+    # mainLayout.addElement(footer)
+    # mainLayout.draw(screen,0,0)
+
+
+
+    mainLayout = Layout(400, 400)
+    mainLayout.draw(screen,0,0)
+
     pygame.display.flip()
 
     clock.tick(60)
