5584144d35c49e70c593c28d5569c356a8311f04
Shayan Tehrani <shayanthrn@gmail.com>
2025-10-17 14:20:46 -0400

    add welcome and rules page

--- main.py
+++ main.py
@@ -20,30 +20,37 @@ class ticTacToeBoard:
 
 
 class UIElement:
-    def __init__(self,width, height, id=0):
+    def __init__(self,width, height, xOffset=0, yOffset=0, id=0):
         self.id = id
         self.width = width
         self.height = height
+        self.xOffset = xOffset
+        self.yOffset = yOffset
     def draw(self,screen,x,y):
-        pygame.draw.rect(screen, (randint(0,255),randint(0,255),randint(0,255)), (x, y, self.width, self.height))
-        font = pygame.font.SysFont(None, 20)
-        text = font.render(str(self.id), True, (0, 0, 0))
-        screen.blit(text, (x + 5, y + 5))
+        pass
 
+class UIImage(UIElement):
+    def __init__(self,width, height, imagePath, xOffset=0, yOffset=0, id=0):
+        super().__init__(width,height,xOffset,yOffset,id)
+        self.image = pygame.image.load(imagePath)
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+    def draw(self,screen,x,y):
+        screen.blit(self.image, (x + self.xOffset, y + self.yOffset))
 
 class UILabel(UIElement):
-    def __init__(self,width, height, text, fontType = None, fontSize=32, id=0):
-        super().__init__(width,height,id)
+    def __init__(self,width, height, text, fontType = None, fontSize=32, xOffset=0, yOffset=0, id=0):
+        super().__init__(width,height,xOffset,yOffset,id)
         self.text = text
         self.font = pygame.font.Font(fontType, fontSize)
 
     def draw(self,screen,x,y):
         text_surface = self.font.render(self.text, True, (0, 0, 0))
-        screen.blit(text_surface, (x, y))
+        screen.blit(text_surface, (x + self.xOffset, y + self.yOffset))
 
 class UIButton(UIElement):
-    def __init__(self,width, height, image, hoverImage, id=0):
-        super().__init__(width,height,id)
+    def __init__(self,width, height, image, hoverImage, xOffset=0, yOffset=0, id=0):
+        super().__init__(width,height,xOffset,yOffset,id)
         self.image = pygame.image.load(image)
         self.image = pygame.transform.scale(self.image, (width, height))
         self.hoverImage = pygame.image.load(hoverImage)
@@ -51,15 +58,15 @@ class UIButton(UIElement):
 
     def draw(self,screen,x,y):
         mousePos = pygame.mouse.get_pos()
-        if x <= mousePos[0] <= x + self.width and y <= mousePos[1] <= y + self.height:
-            screen.blit(self.hoverImage, (x, y))
+        if x + self.xOffset <= mousePos[0] <= x + self.width + self.xOffset and y + self.yOffset <= mousePos[1] <= y + self.height + self.yOffset:
+            screen.blit(self.hoverImage, (x + self.xOffset, y + self.yOffset))
         else:
-            screen.blit(self.image, (x, y))
+            screen.blit(self.image, (x + self.xOffset, y + self.yOffset))
 
 
 class UILayout(UIElement):
-    def __init__(self,width, height,id=0):
-        super().__init__(width,height,id)
+    def __init__(self,width, height,xOffset=0,yOffset=0,id=0):
+        super().__init__(width,height,xOffset,yOffset,id)
         self.elements = []
 
     def addElement(self,element):
@@ -69,9 +76,8 @@ class UILayout(UIElement):
 
 
 class UIVerticalLinearLayout(UILayout):
-
-    def __init__(self,width, height,id=0):
-        super().__init__(width,height,id)
+    def __init__(self,width, height,xOffset=0,yOffset=0,id=0):
+        super().__init__(width,height,xOffset,yOffset,id)
 
     def draw(self,screen,x,y):
         current_y = y
@@ -79,28 +85,67 @@ class UIVerticalLinearLayout(UILayout):
             if(current_y + element.height > y + self.height or element.width > self.width):
                 printError("Element exceeds layout bounds")
             element.draw(screen,x,current_y)
-            current_y += element.height
+            current_y += element.height + element.yOffset
 
 
 class UIHorizontalLinearLayout(UILayout):
+    def __init__(self,width, height,xOffset=0,yOffset=0,id=0):
+        super().__init__(width,height,xOffset,yOffset,id)
 
-    def __init__(self,width, height,id=0):
-        super().__init__(width,height,id)
-        
     def draw(self,screen,x,y):
         current_x = x
         for element in self.elements:
             if(current_x + element.width > x + self.width or element.height > self.height):
                 printError("Element exceeds layout bounds")
             element.draw(screen,current_x,y)
-            current_x += element.width
+            current_x += element.width + element.xOffset
+
+def DescriptionLine(text, fontType = "./assets/Fonts/ethnocentric.ttf", fontSize=12):
+    return UILabel(SCREEN_WIDTH, 20, text, fontType, fontSize,10)
+def HeaderLine(text, fontType = "./assets/Fonts/ethnocentric.ttf", fontSize=48):
+    return UILabel(SCREEN_WIDTH, 80, text, fontType, fontSize,10)
+def Header2Line(text, fontType = "./assets/Fonts/ethnocentric.ttf", fontSize=34):
+    return UILabel(SCREEN_WIDTH, 60, text, fontType, fontSize,10)
+def Header3Line(text, fontType = "./assets/Fonts/ethnocentric.ttf", fontSize=20):
+    return UILabel(SCREEN_WIDTH, 60, text, fontType, fontSize,10)
+
+def GetWelComeAndRulesLayout():
+    layout = UIVerticalLinearLayout(SCREEN_WIDTH,SCREEN_HEIGHT)
+    layout.addElement(HeaderLine("Welcome to Tic Tac Toe!"))
+    layout.addElement(Header2Line("Objective:"))
+    layout.addElement(DescriptionLine("The goal of the game is to win any one mini-board by getting three marks in a row within"))
+    layout.addElement(DescriptionLine("that mini-board."))
+    layout.addElement(Header2Line("Setup:"))
+    layout.addElement(DescriptionLine("1. The game consists of 9 mini Tic Tac Toe boards, arranged in a 3×3 grid."))
+    layout.addElement(DescriptionLine("2. Each mini board is a standard 3×3 Tic Tac Toe board."))
+    layout.addElement(DescriptionLine("3. Players take turns placing their mark (X or O) on the boards."))
+    layout.addElement(DescriptionLine("4. The first move must be made on the center mini-board."))
+    layout.addElement(Header2Line("Gameplay Rules:"))
+    layout.addElement(DescriptionLine("1. On your turn, you must place your mark on the active mini-board."))
+    layout.addElement(DescriptionLine("2. Wherever the previous player placed their mark within a mini-board determines the next "))
+    layout.addElement(DescriptionLine("mini-board the opponent must play in."))
+    layout.addElement(DescriptionLine("3. If the target mini-board is already full, the next player may choose any"))
+    layout.addElement(DescriptionLine("available mini-board."))
+    layout.addElement(DescriptionLine("4. Players alternate turns until one player wins a mini-board."))
+    layout.addElement(Header2Line("Difficulties:"))
+    layout.addElement(Header2Line("Easy:"))
+    layout.addElement(DescriptionLine("Plays completely random moves within the allowed mini-board. No strategy is used."))
+    layout.addElement(Header2Line("Medium:"))
+    layout.addElement(DescriptionLine("Base strategy to block opponent wins and take wins when available."))
+    layout.addElement(Header2Line("Hard:"))
+    layout.addElement(DescriptionLine("Powered by a neural network trained with a genetic algorithm."))
+    layout.addElement(DescriptionLine("If you manage to win against this AI, feel free to reject my job application. I’ll understand."))
+    layout.addElement(UIButton(200, 75, "./assets/buttons/Rect/OkText/Default.png", "./assets/buttons/Rect/OkText/Hover.png", SCREEN_WIDTH/2 - 100))
+    return layout
 
 
 def GetMainMenuLayout():
-    layout = UIVerticalLinearLayout(900,900)
-    layout.addElement(UIButton(150, 50, "./assets/buttons/Rect/PlayIcon/Default.png", "./assets/buttons/Rect/PlayIcon/Hover.png", 1))
-    layout.addElement(UILabel(200, 50, "Reimagined Tic Tac Toe", "./assets/Fonts/ethnocentric.ttf", 32, 2))
-    layout.addElement(UIButton(50, 50, "./assets/Exit.png", "./assets/Exit.png", 1))
+    layout = UIVerticalLinearLayout(SCREEN_WIDTH,SCREEN_HEIGHT)
+    layout.addElement(UILabel(HEADER_TEXT_WIDTH, HEADER_TEXT_HEIGHT, "Main Menu", "./assets/Fonts/ethnocentric.ttf", 48,SCREEN_WIDTH/2 - HEADER_TEXT_WIDTH/2))
+    layout.addElement(UIImage(400,400,"./assets/images/board.png",SCREEN_WIDTH/2 - 200))
+    layout.addElement(UILabel(DIFFICULTY_TEXT_WIDTH, DIFFICULTY_TEXT_HEIGHT, "Select The Difficulty", "./assets/Fonts/ethnocentric.ttf", 48,SCREEN_WIDTH/2 - DIFFICULTY_TEXT_WIDTH/2,50))
+    layout.addElement(UIButton(200, 75, "./assets/buttons/Rect/PlayIcon/Default.png", "./assets/buttons/Rect/PlayIcon/Hover.png", SCREEN_WIDTH/2 - 100))
+    
     return layout
 
 def GetInGameLayout():
@@ -109,6 +154,7 @@ def GetInGameLayout():
     return layout
 
 class GameState(Enum):
+    WELCOME_AND_RULES = 0
     MAIN_MENU = 1
     IN_GAME = 2
 
@@ -116,10 +162,14 @@ class GameState(Enum):
 pygame.init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 900, 900
+HEADER_TEXT_HEIGHT = 100
+HEADER_TEXT_WIDTH = 400
+DIFFICULTY_TEXT_WIDTH = 800
+DIFFICULTY_TEXT_HEIGHT = 100
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Reimagined Tic Tac Toe")
+pygame.display.set_caption("Tic Tac Toe")
 
-game_state = GameState.MAIN_MENU
+game_state = GameState.WELCOME_AND_RULES
 
 clock = pygame.time.Clock()
 
@@ -129,9 +179,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.fill((255, 255, 255))
-    
-    if game_state == GameState.MAIN_MENU:
+    screen.fill((255, 255, 254))
+    if game_state == GameState.WELCOME_AND_RULES:
+        layout = GetWelComeAndRulesLayout()
+    elif game_state == GameState.MAIN_MENU:
         layout = GetMainMenuLayout()
     elif game_state == GameState.IN_GAME:
         layout = GetInGameLayout()
