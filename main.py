bd271e54501be46ffa70b1464067312f9882b6e0
Shayan Tehrani <shayanthrn@gmail.com>
2025-10-17 15:37:03 -0400

    add bg music

--- main.py
+++ main.py
@@ -199,17 +199,38 @@ class GameManager:
     def __init__(self):
         self.difficulty = None
         self.gameState = GameState.WELCOME_AND_RULES
+        self.bg_music_menu = "./assets/Musics/bg_menu.mp3"
+        self.bg_music_ingame = "./assets/Musics/bg_ingame.mp3"
+        pygame.mixer.music.load(self.bg_music_menu)
+        pygame.mixer.music.set_volume(0.3)
+        pygame.mixer.music.play(-1)
 
     def getGameState(self):
         return self.gameState
     
     def setGameState(self, state):
         self.gameState = state
+        self.updateBackgroundMusic()
     
     def setDifficultyAndStart(self, difficulty):
         self.difficulty = difficulty
+        self.setGameState(GameState.IN_GAME)
+
+    def updateBackgroundMusic(self):
+        if self.gameState in [GameState.WELCOME_AND_RULES, GameState.MAIN_MENU]:
+            pygame.mixer.music.load(self.bg_music_menu)
+            pygame.mixer.music.play(-1)
+
+        elif self.gameState == GameState.IN_GAME:
+            pygame.mixer.music.load(self.bg_music_ingame)
+            pygame.mixer.music.play(-1)
 
 pygame.init()
+pygame.mixer.init()
+
+
+
+
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 900, 900
 HEADER_TEXT_HEIGHT = 100
@@ -226,6 +247,8 @@ clock = pygame.time.Clock()
 welcomeLayout = GetWelComeAndRulesLayout()
 mainMenuLayout = GetMainMenuLayout()
 inGameLayout = GetInGameLayout()
+
+
 while True:
     screen.fill((255, 255, 254))
 
